0dc88fce5fe741a3b7dc1be20b349c619695eaa7
drewgoenner <52584951+drewgoenner@users.noreply.github.com>
2019-08-06 15:23:35 -0500

    created a few addEventListeners

--- js/index.js
+++ js/index.js
@@ -1,1 +1,30 @@
 // Your code goes here
+
+// const noNav = document.querySelectorAll('.nav-link');
+// noNav.addEventListener('click', (e) => {
+//     console.log('Not Getting Anywhere This Way!');
+//     event.preventDefault();
+// })
+
+const funBusImg = document.querySelector('.home .intro img');
+funBusImg.addEventListener('contextmenu', (e) => {
+    alert("You ain't left, cause that was so right!");
+    e.stopPropagation();
+})
+
+const funBusIntro = document.querySelector('.home .intro');
+funBusIntro.addEventListener('mouseenter', (e) => {
+    funBusIntro.style.border = '2px solid red';
+})
+funBusIntro.addEventListener('mouseleave', (e) => {
+    funBusIntro.style.border = 'none';
+})
+funBusIntro.addEventListener('contextmenu', (e) => {
+    alert("Stop peeking under the hood!");
+})
+
+
+const funBusH2 = document.querySelector('h2');
+funBusH2.addEventListener('scroll', (e) => {
+    funBusH2.textContent = '8rem';
+})
