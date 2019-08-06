7503a45d0e63ea5790b33490e95115c5f9f5d6f0
drewgoenner <52584951+drewgoenner@users.noreply.github.com>
2019-08-06 16:34:49 -0500

    completed MVP for project

--- js/index.js
+++ js/index.js
@@ -1,10 +1,10 @@
 // Your code goes here
 
-// const noNav = document.querySelectorAll('.nav-link');
-// noNav.addEventListener('click', (e) => {
-//     console.log('Not Getting Anywhere This Way!');
-//     event.preventDefault();
-// })
+const noNav = document.querySelector('.nav');
+noNav.addEventListener('click', (e) => {
+    console.log('Not Getting Anywhere This Way!');
+    event.preventDefault();
+})
 
 const funBusImg = document.querySelector('.home .intro img');
 funBusImg.addEventListener('contextmenu', (e) => {
@@ -24,7 +24,41 @@ funBusIntro.addEventListener('contextmenu', (e) => {
 })
 
 
-const funBusH2 = document.querySelector('h2');
-funBusH2.addEventListener('scroll', (e) => {
-    funBusH2.textContent = '8rem';
+const funAdv = document.querySelector('.home .content-section .img-content img');
+funAdv.addEventListener('drag', (e) => {
+    funAdv.style.display = "none";
 })
+
+const noIntro = document.querySelector('.home .inverse-content .img-content img');
+noIntro.addEventListener('dblclick', (e) => {
+    funBusIntro.style.display = "none";
+})
+
+const noPage = document.querySelector('html');
+noPage.addEventListener('wheel', (e) => {
+    noPage.style.display = "none";
+})
+
+const bigText = document.querySelector('.home .content-section .text-content h2');
+bigText.addEventListener('click', (e) => {
+    bigText.style.fontSize = '50px';
+})
+
+const copyDoom = document.querySelector('.home .content-section .text-content p');
+copyDoom.addEventListener('copy', (e) => {
+    copyDoom.textContent="DOOM DOOM DOOM DOOM DOOM DOOM DOOM DOOM"
+    copyDoom.style.fontSize = '3rem';
+})
+
+const yellowTrip = document.querySelectorAll('.destination');
+yellowTrip.forEach(makeYellow => {
+    makeYellow.addEventListener('click', (e) =>{
+        makeYellow.style.backgroundColor = 'yellow';
+    })
+})
+const tripButton = document.querySelectorAll('.btn');
+tripButton.forEach(makeGreen => {
+    makeGreen.addEventListener('click', (e) => {
+        makeGreen.style.backgroundColor = 'green';
+    })
+});
